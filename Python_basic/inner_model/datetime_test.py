eb506ce33a579bf968def10596770e399c271761
Simba <haixiaomei@qiyi.com>
2022-01-14 18:07:17 +0800

    update datetime model test

--- Python_basic/inner_model/datetime_test.py
+++ Python_basic/inner_model/datetime_test.py
@@ -26,11 +26,15 @@ def test():
     tsp = 1666666666.0
     tsp2dt = datetime.fromtimestamp(tsp)  # local time
     tsp2utc = datetime.utcfromtimestamp(tsp)  # UTC time
+    str2dtm = datetime.strptime('2022-01-14 18:08:18', '%Y-%m-%d %H:%M:%S')
+    dtm2str = now.strftime('%a, %b %d %H:%M')  # '%a, %b %d %H:%M'
 
     logging.info('The pointed time is: %s ' % dt)
     logging.info('Timestamp is: %s ' % dt_timestamp)
     logging.info('Timestamp %s to local Peking datetime is %s ' % (tsp, tsp2dt))
     logging.info('Timestamp %s to UTC datetime is %s ' % (tsp, tsp2utc))
+    logging.info('String to datetime is %s ' % str2dtm)
+    logging.info('Datetime now %s to string is %s ' % (now, dtm2str))
 
 
 if __name__ == '__main__':
@@ -44,5 +48,7 @@ if __name__ == '__main__':
         INFO:root:Timestamp is: 1642131540.0 
         INFO:root:Timestamp 1666666666.0 to datetime is 2022-10-25 10:57:46 
         INFO:root:Timestamp 1666666666.0 to UTC datetime is 2022-10-25 02:57:46 
+        INFO:root:String to datetime is 2022-01-14 18:08:18 
+        INFO:root:Datetime now 2022-01-14 18:06:23.704516 to string is Fri, Jan 14 18:06 
 
     """
