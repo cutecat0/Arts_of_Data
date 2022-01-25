1371330010b4dbf04d3387c60bf65ea1ff068543
Simba <17854295270@163.com>
2022-01-25 17:49:44 +0800

    add new inner model test

---
+++ Python_basic/inner_model/contextlib_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import logging
+import itertools
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+def contextlib_test():
+    """
+    try...catch...finally is too much busy...
+    use with can be more easily
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+
+    contextlib_test()
