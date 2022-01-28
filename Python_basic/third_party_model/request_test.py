a226ea98014f054615d7b0866ea883542d985628
Simba <37787934+cutecat0@users.noreply.github.com>
2022-01-28 11:08:27 +0800

    update request test

--- Python_basic/third_party_model/request_test.py
+++ Python_basic/third_party_model/request_test.py
@@ -17,11 +17,25 @@ def test(url):
     logging.info(f'Status: {req.status_code}')
     logging.info(f'Text : {req.text}')
 
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Mobile Safari/537.36'
+}
+
+
+def test1():
+    req = requests.get('https://www.liaoxuefeng.com/wiki/1016959663602400/1183249464292448', params={'q': 'python', 'cat': '1001'})
+    logging.info(f'real url is : {req.url}')
+
+    logging.info(f'requests encoding: {req.encoding}')
+
+    content = req.content
+    logging.info(f'request content is: {content}')
+
 
 if __name__ == '__main__':
     url = 'https://www.liaoxuefeng.com/wiki/1016959663602400/1183249464292448'
     url = 'https://github.com/cutecat0'
-    test(url)
+    # test(url)
     """
     INFO:root:Status: 200
     INFO:root:Text : 
@@ -30,3 +44,8 @@ if __name__ == '__main__':
     ...
     ...
     """
+
+    test1()
+    # INFO:root:real url is : https://www.douban.com/search?q=python&cat=1001
+    # INFO:root:requests encoding: None
+
