2eeecec16df14c2f51ce4b77feba4a729e43a8af
Simba <37787934+cutecat0@users.noreply.github.com>
2022-01-28 18:20:46 +0800

    add network test

---
+++ Python_basic/network_process/network_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+def test():
+    """
+    network connection message pass is actually two process of the two different computers
+    :return:
+    """
+
---
+++ Python_basic/network_process/tcp_ip_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+def test():
+    """
+    TCP/IP protocol
+    only tag is IP address on the Internet to transfer message between two or more computers
+    :return:
+    """
