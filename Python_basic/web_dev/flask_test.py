90267c25125476186375aecce4db7f78d29c5a66
Simba <37787934+cutecat0@users.noreply.github.com>
2022-03-18 20:22:29 +0800

    add more e.g. of falsk test

--- Python_basic/web_dev/flask_test.py
+++ Python_basic/web_dev/flask_test.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
 import logging
 from flask import Flask
+from flask import request
 
 logging.getLogger().setLevel(logging.INFO)
 
 app = Flask(__name__)
 
 
-@app.route('/')
+# @app.route('/')
 def powers(n=10):
     return ','.join(str(2 ** i) for i in range(n))
 
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return '<h1>Home</h1>'
+
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+
+
+@app.route('/signin', methods=['POST'])
+def sign():
+    # 需要从request对象读取表单内容：
+    if request.form['username'] == 'admin' and request.form['password'] == 'password':
+        return '<h3>Hello, admin!</h3>'
+    return '<h3>Bad username or password.</h3>'
+
+
 if __name__ == '__main__':
     app.run()
-
-
--- Python_basic/web_dev/readme.md
+++ Python_basic/web_dev/readme.md
@@ -11,3 +11,20 @@ one HTTP request only deal one source<br>
 
 
 ###WSGI: Web Server Gateway Interface<br>
+
+Flask
+<br>Flask is one of the Web Struct tools <br>
+由于用Python开发一个Web框架十分容易，所以Python有上百个开源的Web框架。
+这里我们先不讨论各种Web框架的优缺点，直接选择一个比较流行的Web框架——Flask来使用。
+
+用Flask编写Web App比WSGI接口简单
+
+除了Flask，常见的Python Web框架还有：
+
+Django：全能型Web框架；
+
+web.py：一个小巧的Web框架；
+
+Bottle：和Flask类似的Web框架；
+
+Tornado：Facebook的开源异步Web框架。
