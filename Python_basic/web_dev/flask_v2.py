8da9d9484a138f47eea27e7c373fc94d82a71e6e
Simba <37787934+cutecat0@users.noreply.github.com>
2022-03-21 21:21:23 +0800

    add Jinja2 html template e.g.

--- Python_basic/web_dev/flask_v2.py
+++ Python_basic/web_dev/flask_v2.py
@@ -14,17 +14,25 @@ def home():
     '''
     use template then pass parameters in template html
     which is MVC : Model-View-Controller
+    use Jinja2 as html template in this eg
+
+    除了Jinja2，常见的模板还有：
+    Mako：用<% ... %>和${xxx}的一个模板；
+    Cheetah：也是用<% ... %>和${xxx}的一个模板；
+    Django：Django是一站式框架，内置一个用{% ... %}和{{ xxx }}的模板。
+    小结
+    有了MVC，我们就分离了Python代码和HTML代码。HTML代码全部放到模板里，写起来更有效率。
     :return:
     '''
     return render_template('home.html')
 
 
-@app.route('/signin', methods='[GET]')
+@app.route('/signin', methods=['GET'])
 def signin_form():
     return render_template('form.html')
 
 
-@app.route('/signin', methods='[POST]')
+@app.route('/signin', methods=['POST'])
 def signin():
     username = request.form['username']
     pwd = request.form['password']
