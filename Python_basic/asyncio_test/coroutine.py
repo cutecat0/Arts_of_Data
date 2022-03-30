1bc21f90ca999dcbf7641abad3dfcb643c7d5165
Simba <37787934+cutecat0@users.noreply.github.com>
2022-03-30 20:59:09 +0800

    add asyncio test

---
+++ Python_basic/asyncio_test/coroutine.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+class CoroutineTest(object):
+
+    def __init__(self):
+        """
+        协程，又称微线程，纤程。
+        英文名Coroutine。
+        https://www.liaoxuefeng.com/wiki/1016959663602400/1017968846697824
+        """
