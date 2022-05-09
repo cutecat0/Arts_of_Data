24de9f8f35d4f38506ca9a3e34eb01848c60d5f7
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-09 19:03:56 +0800

    update Counter()

--- Python_basic/cookbook_practice/data_struct_algorithm/12_most_times_element.py
+++ Python_basic/cookbook_practice/data_struct_algorithm/12_most_times_element.py
@@ -17,7 +17,7 @@ def most_times_in_series():
     pet_counts = Counter(pets)
 
     top_3_pets = pet_counts.most_common(3)
-    print(top_3_pets)
+    # print(top_3_pets)
     # [('cat', 3), ('Simba', 2), ('loves', 2)]
 
     more_pets = [
@@ -27,11 +27,27 @@ def most_times_in_series():
     for pet in more_pets:
         pet_counts[pet] += 1
 
-    print(pet_counts['Cat'])  # 3
+    # print(pet_counts['Cat'])  # 3
 
     pet_counts.update(more_pets)   # same function as above  each word plus 1 here
-    print(pet_counts)
+    # print(pet_counts)
     # Counter({'duck': 4, 'Cat': 4, 'cat': 3, 'rabbit': 3, 'say': 3, 'no': 3, 'Simba': 2, 'loves': 2, 'so': 1, 'much': 1, 'Kitty': 1})
+
+    a = Counter(pets)
+    print(a)
+    Counter({'cat': 3, 'Simba': 2, 'loves': 2, 'duck': 2, 'Cat': 2, 'so': 1, 'much': 1, 'rabbit': 1, 'say': 1, 'no': 1, 'Kitty': 1})
+
+    b = Counter(more_pets)
+    print(b)
+    Counter({'Cat': 1, 'rabbit': 1, 'duck': 1, 'say': 1, 'no': 1})
+
+    c = a + b
+    print(c)
+    Counter({'cat': 3, 'duck': 3, 'Cat': 3, 'Simba': 2, 'loves': 2, 'rabbit': 2, 'say': 2, 'no': 2, 'so': 1, 'much': 1, 'Kitty': 1})
+
+    d = a - b
+    print(d)
+    Counter({'cat': 3, 'Simba': 2, 'loves': 2, 'so': 1, 'much': 1, 'duck': 1, 'Kitty': 1, 'Cat': 1})
 
 
 if __name__ == '__main__':
