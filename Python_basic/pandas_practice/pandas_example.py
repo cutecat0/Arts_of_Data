3f05ca832e32ddc050ab812584d073915a29b589
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-10 17:20:07 +0800

    update pd

--- Python_basic/pandas_practice/pandas_example.py
+++ Python_basic/pandas_practice/pandas_example.py
@@ -71,6 +71,55 @@ def pandas_practice():
     # 3    Duck     3.1      -1      88  thin
     # 4  Rabbit     3.8      -1      98  thin
 
+    df_1 = df.loc[(df.weight <= 6) & (df.lovely >= 100), 'weight']
+    # print(df_1)
+    #     0    4.2
+    # Name: weight, dtype: float64
+
+    df_2 = df.loc[(df.weight <= 6) | (df.lovely >= 100), 'weight']
+    # print(df_2)
+    # 0     4.2
+    # 1     6.6
+    # 2    12.5
+    # 3     3.1
+    # 4     3.8
+
+    df.loc[(df.weight <= 6) & (df.lovely >= 100), 'weight'] = 6
+    # print(df)
+    #     name  weight  height  lovely  logic
+    # 0     Cat     6.0      -1     100   thin
+    # 1   Kitty     6.6     666     666  heavy
+    # 2     Dog    12.5     666     666  heavy
+    # 3    Duck     3.1      -1      88   thin
+    # 4  Rabbit     3.8      -1      98   thin
+
+    num = 20
+    df_3 = df.loc[(df.lovely - num).abs().argsort()]
+    # print(df_3)
+    #     name  weight  height  lovely  logic
+    # 3    Duck     3.1      -1      88   thin
+    # 4  Rabbit     3.8      -1      98   thin
+    # 0     Cat     6.0      -1     100   thin
+    # 1   Kitty     6.6     666     666  heavy
+    # 2     Dog    12.5     666     666  heavy
+
+    sub_df1 = df.weight <= 6
+    sub_df2 = df.height == -1
+    sub_df3 = df.lovely < 100
+    all_sub_df = sub_df1 & sub_df2 & sub_df3
+    # print(df[all_sub_df])
+    #     name  weight  height  lovely logic
+    # 3    Duck     3.1      -1      88  thin
+    # 4  Rabbit     3.8      -1      98  thin
+
+    import functools
+    sub_df_list = [sub_df1, sub_df2, sub_df3]
+    all_sub_df_v2 = functools.reduce(lambda x, y: x & y, sub_df_list)
+    # print(df[all_sub_df_v2])
+    #     name  weight  height  lovely logic
+    # 3    Duck     3.1      -1      88  thin
+    # 4  Rabbit     3.8      -1      98  thin
+
 
 if __name__ == '__main__':
 
