87ef525ecf7a8bb5e56fd0c33d19f4ed63ff5844
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-10 18:34:00 +0800

    update index lable of pd

--- Python_basic/pandas_practice/pandas_example.py
+++ Python_basic/pandas_practice/pandas_example.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def pandas_practice():
+def df_use():
     pets = {
         'name': ['Cat', 'Kitty', 'Dog', 'Duck', 'Rabbit'],
         'weight': [4.2, 6.6, 12.5, 3.1, 3.8],
@@ -120,7 +120,72 @@ def pandas_practice():
     # 3    Duck     3.1      -1      88  thin
     # 4  Rabbit     3.8      -1      98  thin
 
+    df_selected = df[(df.weight >= 6.0) & (df.index.isin([0, 2, 4]))]
+    # print(df_selected)
+    #     name  weight  height  lovely  logic
+    # 0  Cat     6.0      -1     100   thin
+    # 2  Dog    12.5     666     666  heavy
+
+    # print(df)
+    #     name  weight  height  lovely  logic
+    # 0     Cat     6.0      -1     100   thin
+    # 1   Kitty     6.6     666     666  heavy
+    # 2     Dog    12.5     666     666  heavy
+    # 3    Duck     3.1      -1      88   thin
+    # 4  Rabbit     3.8      -1      98   thin
+    new_df = pd.DataFrame(pets,
+                          index=['aa', 'bb', 'cc', 'dd', 'ee'])
+    # print(new_df)
+    #     name  weight  height  lovely
+    # aa     Cat     4.2       6     100
+    # bb   Kitty     6.6       7     -20
+    # cc     Dog    12.5      20      99
+    # dd    Duck     3.1       3      88
+    # ee  Rabbit     3.8       5      98
+    ndf1 = new_df.loc["aa": "cc"]
+    # print(ndf1)
+    #     name  weight  height  lovely
+    # aa    Cat     4.2       6     100
+    # bb  Kitty     6.6       7     -20
+    # cc    Dog    12.5      20      99
+
+    ndf2 = new_df[0:3]
+    # print(ndf2)
+    #     name  weight  height  lovely
+    # aa    Cat     4.2       6     100
+    # bb  Kitty     6.6       7     -20
+    # cc    Dog    12.5      20      99
+
+    ndf3 = new_df["aa":"cc"]
+    # print(ndf3)
+    #     name  weight  height  lovely
+    # aa    Cat     4.2       6     100
+    # bb  Kitty     6.6       7     -20
+    # cc    Dog    12.5      20      99
+
+    new_df2 = pd.DataFrame(pets,
+                           index=[1, 2, 3, 4, 5])  # here index from 1 not 0 start  label
+    # print(new_df2)
+    #     name  weight  height  lovely
+    # 1     Cat     4.2       6     100
+    # 2   Kitty     6.6       7     -20
+    # 3     Dog    12.5      20      99
+    # 4    Duck     3.1       3      88
+    # 5  Rabbit     3.8       5      98
+
+    ndf2_1 = new_df2.iloc[1:3]  # position-oriented
+    print(ndf2_1)
+    #     name  weight  height  lovely
+    # 2  Kitty     6.6       7     -20
+    # 3    Dog    12.5      20      99
+
+    ndf2_2 = new_df2.loc[1:3]  # label-oriented
+    print(ndf2_2)
+    #     name  weight  height  lovely
+    # 1    Cat     4.2       6     100
+    # 2  Kitty     6.6       7     -20
+    # 3    Dog    12.5      20      99
+
 
 if __name__ == '__main__':
-
-    pandas_practice()
+    df_use()
