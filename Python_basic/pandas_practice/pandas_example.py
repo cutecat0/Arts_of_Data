8ac76e805c362d75f30798dc7bfd88e230c6a5f9
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-10 16:27:31 +0800

    add if-else-then of pd and np

--- Python_basic/pandas_practice/pandas_example.py
+++ Python_basic/pandas_practice/pandas_example.py
@@ -37,6 +37,40 @@ def pandas_practice():
     # 3    Duck     3.1      -1      88
     # 4  Rabbit     3.8      -1      98
 
+    mask = pd.DataFrame(
+        {'name': [True] * 4,
+         'weight': [False] * 4,
+         'height': [True, False] * 2,
+         'lovely': [True, False] * 2
+         }
+    )
+    mask_df = df.where(mask, -9999)
+    # print(mask_df)
+    #     name  weight  height  lovely
+    # 0    Cat -9999.0      -1     100
+    # 1  Kitty -9999.0   -9999   -9999
+    # 2    Dog -9999.0     666     666
+    # 3   Duck -9999.0   -9999   -9999
+    # 4  -9999 -9999.0   -9999   -9999
+
+    df["logic"] = np.where(df['weight'] > 6, 'heavy', 'thin')
+    # print(df)
+    #     name  weight  height  lovely  logic
+    # 0     Cat     4.2      -1     100   thin
+    # 1   Kitty     6.6     666     666  heavy
+    # 2     Dog    12.5     666     666  heavy
+    # 3    Duck     3.1      -1      88   thin
+    # 4  Rabbit     3.8      -1      98   thin
+
+    # print(df[df.lovely > 100])
+    #     name  weight  height  lovely  logic
+    # 1  Kitty     6.6     666     666  heavy
+    # 2    Dog    12.5     666     666  heavy
+    # print(df[df.weight < 4])
+    #     name  weight  height  lovely logic
+    # 3    Duck     3.1      -1      88  thin
+    # 4  Rabbit     3.8      -1      98  thin
+
 
 if __name__ == '__main__':
 
