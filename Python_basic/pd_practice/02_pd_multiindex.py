3204f185a5520c08d21f0ea10b63e4c9106fd9b3
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-12 17:51:15 +0800

    add df broadcast through muiltiIndex

--- Python_basic/pd_practice/02_pd_multiindex.py
+++ Python_basic/pd_practice/02_pd_multiindex.py
@@ -4,17 +4,23 @@ import numpy as np
 
 def pd_eg():
     cols = pd.MultiIndex.from_tuples(
-        [(x, y) for x in ['A', 'B', 'C'] for y in ['0', '1']]
+        [(x, y) for x in ['A', 'B', 'C'] for y in ['o', 'I']]
     )
 
     df = pd.DataFrame(np.random.randn(2, 6), index=['m', 'n'], columns=cols)
     print(df)
+    #     A                   B                   C
+    #     o         I         o         I         o         I
+    # m -1.647612 -0.124666 -1.100841 -0.007436  0.796428  0.132117
+    # n -0.645900 -0.182829  0.485222  1.143299 -0.200310  1.492418
 
+    df = df.div(df["C"], level=1)
+    print(df)
+    #     A                    B              C
+    # o         I          o         I    o    I
+    # m  0.843623 -2.473784  -0.039459 -2.159385  1.0  1.0
+    # n -3.912239  0.317357  12.643313  0.004393  1.0  1.0
 
-#      A                   B                   C
-# 0         1         0         1         0         1
-# m -1.718251 -0.911251 -0.312013 -0.894137 -2.356291  0.848239
-# n  0.163396 -0.156341 -0.607056 -0.232332 -0.253261 -1.823689
 
 
 if __name__ == '__main__':
