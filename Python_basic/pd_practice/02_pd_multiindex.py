875a06b62fddb1d6acd169a1578d07800dc51aab
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-12 18:05:04 +0800

    update pd

--- Python_basic/pd_practice/02_pd_multiindex.py
+++ Python_basic/pd_practice/02_pd_multiindex.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def pd_eg():
+def pd_arithmetic():
     cols = pd.MultiIndex.from_tuples(
         [(x, y) for x in ['A', 'B', 'C'] for y in ['o', 'I']]
     )
@@ -22,6 +22,32 @@ def pd_eg():
     # n -3.912239  0.317357  12.643313  0.004393  1.0  1.0
 
 
+def pd_slice():
+
+    tup = [
+        ('AA', 'one'),
+        ('AA', 'two'),
+        ('BB', 'one'),
+        ('BB', 'two'),
+        ('BB', 'three')
+    ]
+
+    index = pd.MultiIndex.from_tuples(tup)
+
+    df = pd.DataFrame([11, 22, 33, 44, 55], index, ['mData'])
+    print(df)
+    """
+              mData
+    AA one       11
+       two       22
+    BB one       33
+       two       44
+       three     55
+
+    """
+
 
 if __name__ == '__main__':
-    pd_eg()
+
+    # pd_arithmetic()
+    pd_slice()
