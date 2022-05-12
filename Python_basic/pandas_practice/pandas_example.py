0efec822cf8b2bb68c8edafae8f7b675963d2970
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-12 16:37:27 +0800

    update pd columns operatitions

--- Python_basic/pandas_practice/pandas_example.py
+++ Python_basic/pandas_practice/pandas_example.py
@@ -174,18 +174,133 @@ def df_use():
     # 5  Rabbit     3.8       5      98
 
     ndf2_1 = new_df2.iloc[1:3]  # position-oriented
-    print(ndf2_1)
+    # print(ndf2_1)
     #     name  weight  height  lovely
     # 2  Kitty     6.6       7     -20
     # 3    Dog    12.5      20      99
 
     ndf2_2 = new_df2.loc[1:3]  # label-oriented
-    print(ndf2_2)
+    # print(ndf2_2)
     #     name  weight  height  lovely
     # 1    Cat     4.2       6     100
     # 2  Kitty     6.6       7     -20
     # 3    Dog    12.5      20      99
 
+    # print(df)
+    other_df = df[~((df.weight < 6) & (df.lovely < 100))]
+    # print(other_df)
+    #     name  weight  height  lovely  logic
+    # 0    Cat     6.0      -1     100   thin
+    # 1  Kitty     6.6     666     666  heavy
+    # 2    Dog    12.5     666     666  heavy
+
+
+def df_use_2():
+    items = {
+        'AA': [1, 2, 1, 1],
+        'BB': [1, 2, 1, 2],
+        'CC': [1, 2, 3, 3],
+        'DD': [1, 2, 3, 4]
+    }
+
+    df = pd.DataFrame(items)
+    # print(df)
+    #     AA  BB  CC  DD
+    # 0   1   1   1   1
+    # 1   2   2   2   2
+    # 2   1   1   3   3
+    # 3   1   2   3   4
+
+    source_cols = df.columns
+    new_cols = [str(x) + '_cat' for x in source_cols]
+    categories = {1: 'Cat', 2: 'Dog', 3: 'Duck', 4: 'Rubbit'}
+    df[new_cols] = df.applymap(categories.get)
+    # print(df)
+    #     AA  BB  CC  DD AA_cat BB_cat CC_cat  DD_cat
+    # 0   1   1   1   1    Cat    Cat    Cat     Cat
+    # 1   2   2   2   2    Dog    Dog    Dog     Dog
+    # 2   1   1   3   3    Cat    Cat   Duck    Duck
+    # 3   1   2   3   4    Cat    Dog   Duck  Rubbit
+
+    df = pd.DataFrame({
+        'AA': [1, 2, 3, 1, 1, 2, 1, 3, 1],
+        'BB': [0, 1, 3, 2, 1, 6, 0, 9, 1]
+    })
+    # print(df)
+    df_idxmin = df.loc[df.groupby('AA')['BB'].idxmin()]
+    # print(df_idxmin)
+    #     AA  BB
+    # 0   1   0
+    # 1   2   1
+    # 2   3   3
+    # sort then take first for each
+    sort_df = df.sort_values(by='BB').groupby('AA', as_index=False).first()
+    print(sort_df)
+    #     AA  BB
+    # 0   1   0
+    # 1   2   1
+    # 2   3   3
+
+
+def df_use_3():
+    df = pd.DataFrame(
+        {
+            'id': [0, 1, 2],
+            'x_a': [1.1, 1.1, 1.1],
+            'x_b': [1.2, 1.2, 1.2],
+            'y_a': [1.11, 1.11, 1.11],
+            'y_b': [1.22, 1.22, 1.22]
+        }
+    )
+    # print(df)
+    #     id   x1   y1    x2    y2
+    # 0   0  1.1  1.2  1.11  1.22
+    # 1   1  1.1  1.2  1.11  1.22
+    # 2   2  1.1  1.2  1.11  1.22
+    # as Labelled index
+    df = df.set_index('id')
+    # print(df)
+    #     x1   y1    x2    y2
+    # id
+    # 0   1.1  1.2  1.11  1.22
+    # 1   1.1  1.2  1.11  1.22
+    # 2   1.1  1.2  1.11  1.22
+
+    df.columns = pd.MultiIndex.from_tuples([tuple(c.split("_")) for c in df.columns])
+    # print(df)
+    #     x          y
+    # a    b     a     b
+    # id
+    # 0   1.1  1.2  1.11  1.22
+    # 1   1.1  1.2  1.11  1.22
+    # 2   1.1  1.2  1.11  1.22
+
+    df = df.stack(0).reset_index(1)
+    # print(df)
+    #     level_1     a     b
+    # id
+    # 0        x  1.10  1.20
+    # 0        y  1.11  1.22
+    # 1        x  1.10  1.20
+    # 1        y  1.11  1.22
+    # 2        x  1.10  1.20
+    # 2        y  1.11  1.22
+    # fix the labels (the label 'level_1' got added automatically)
+    df.columns = ['col_xy', 'all_a', 'all_b']
+    print(df)
+    #     col_xy  all_a  all_b
+    # id
+    # 0       x   1.10   1.20
+    # 0       y   1.11   1.22
+    # 1       x   1.10   1.20
+    # 1       y   1.11   1.22
+    # 2       x   1.10   1.20
+    # 2       y   1.11   1.22
+
 
 if __name__ == '__main__':
-    df_use()
+    # df_use()
+
+    # df_use_2()
+
+    df_use_3()
