4188b1be6328cd2a9a2a79a441e3050e92190703
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-27 18:30:58 +0800

    add histogram

--- data_science/data_analysis/visualization_examples/plot_basic.py
+++ data_science/data_analysis/visualization_examples/plot_basic.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def pic():
+def line_dot_pic():
     year = [2001, 2010, 2020, 2030]
     pop = [2.519, 3.629, 5.263, 6.972]
 
@@ -9,8 +9,26 @@ def pic():
     plt.show()
 
     plt.scatter(year, pop)  # dot plot
+    plt.xscale('log')
+    plt.show()
+
+
+def histogram_pic():
+    values = [0, 0.6, 1.2, 1.3, 2.2, 2.6, 3.3, 3.5, 3.9, 4.6, 6.6]
+
+    plt.hist(values, bins=3)
+    plt.show()
+    plt.clf()
+
+    plt.hist(values)  # bins default is 10
+    plt.show()
+    plt.clf()
+
+    plt.hist(values, bins=5)
     plt.show()
 
 
 if __name__ == '__main__':
-    pic()
+
+    # line_dot_pic()
+    histogram_pic()
