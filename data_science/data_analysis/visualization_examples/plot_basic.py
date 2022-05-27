18cd4585980f3853e01d2d187e07c5c1d8b5ca0b
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-27 19:20:36 +0800

    add customization

--- data_science/data_analysis/visualization_examples/plot_basic.py
+++ data_science/data_analysis/visualization_examples/plot_basic.py
@@ -2,14 +2,27 @@ import matplotlib.pyplot as plt
 
 
 def line_dot_pic():
-    year = [2001, 2010, 2020, 2030]
-    pop = [2.519, 3.629, 5.263, 6.972]
+    year = [2001, 2010, 2020, 2030, 2050, 2100]
+    pop = [2.519, 3.629, 5.263, 6.972, 7.222, 10.525]
+
+    year = [1800, 1850, 1900] + year
+    pop = [1.0, 1.262, 1.650] + pop
 
     plt.plot(year, pop)  # line plot
+
+    # customization
+    plt.xlabel('Year')
+    plt.ylabel('Population')
+
+    plt.title('World Population Projections')
+
+    plt.yticks([i for i in range(14) if i % 2 == 0],
+               [str(i) + 'B' for i in range(14) if i % 2 == 0])
+
     plt.show()
 
     plt.scatter(year, pop)  # dot plot
-    plt.xscale('log')
+    # plt.xscale('log')
     plt.show()
 
 
@@ -30,5 +43,5 @@ def histogram_pic():
 
 if __name__ == '__main__':
 
-    # line_dot_pic()
-    histogram_pic()
+    line_dot_pic()
+    # histogram_pic()
