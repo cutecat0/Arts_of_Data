eeb729ef5d3f5113b77a00fe7b50bb0f7a27a063
Simba <37787934+cutecat0@users.noreply.github.com>
2022-05-30 19:05:29 +0800

    hist test

--- data_science/data_analysis/visualization_examples/seaborn_basic.py
+++ data_science/data_analysis/visualization_examples/seaborn_basic.py
@@ -34,13 +34,30 @@ def anscomde_example():
     fig.show()
 
 
-def seaborn_example():
+def hist_plot():
     tips = sns.load_dataset('tips')
-    print(tips.head())
+    # print(tips.head())
+    """
+       total_bill   tip     sex smoker  day    time  size
+0       16.99  1.01  Female     No  Sun  Dinner     2
+1       10.34  1.66    Male     No  Sun  Dinner     3
+2       21.01  3.50    Male     No  Sun  Dinner     3
+3       23.68  3.31    Male     No  Sun  Dinner     2
+4       24.59  3.61  Female     No  Sun  Dinner     4
+    """
+    fig = plt.figure()
+
+    axes1 = fig.add_subplot(1, 1, 1)
+    axes1.hist(tips['total_bill'], bins=12)
+    axes1.set_title('Histogram of Total Bill')
+    axes1.set_xlabel('Frequency')
+    axes1.set_ylabel('Total Bill')
+
+    fig.show()
 
 
 if __name__ == '__main__':
 
     # anscomde_example()
 
-    seaborn_example()
+    hist_plot()
