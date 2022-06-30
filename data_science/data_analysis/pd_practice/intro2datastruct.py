5adb19f364834f26fed08beb78d7c5e51e145647
Simba <37787934+cutecat0@users.noreply.github.com>
2022-06-30 17:23:36 +0800

    introduce to Data Struct

---
+++ data_science/data_analysis/pd_practice/intro2datastruct.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+
+
+def series_from_ndarray():
+    """
+    A  Series is one-dimensional labeled array.
+    s = pd.Series(data, index=index)
+    """
+    # 1. From ndarray
+    # If data is ndarray, index must be the same length as data.
+    s = pd.Series(np.random.randn(5), index=["a", "b", "c", "d", "e"])
+    print(s)
+    """
+    a   -0.261770
+    b   -0.164276
+    c    0.590781
+    d   -1.468494
+    e   -0.107874
+    dtype: float64
+    """
+    print(s.index)
+    " Index(['a', 'b', 'c', 'd', 'e'], dtype='object') "
+
+
+def series_from_dict():
+    """
+    When the data is a dict and index isn't passed, the index of Series will be ordered by the dict's insertion order.
+    """
+    dic = {"cat": 100, "Duck": 98, "Rabbit": 88}
+    s = pd.Series(dic)
+
+    print(s)
+    """
+    cat       100
+    Duck       98
+    Rabbit     88
+    dtype: int64
+    """
+    """ If an index is passed, the values in the dict will be instead of by passed index """
+    s2 = pd.Series(dic, index=["c", "d", "r", "o"])
+    print(s2)
+
+
+if __name__ == '__main__':
+
+    # series_from_ndarray()
+    series_from_dict()
