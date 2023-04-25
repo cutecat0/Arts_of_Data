c734a1e86801d06656912d7c16ef1cc38fa7dd9a
Simba <37787934+cutecat0@users.noreply.github.com>
2023-04-25 17:56:44 +0800

    update get subdict from a dict

--- data_science/python_part_basic/cookbook_practice/data_struct_algorithm/17_get_subset_from_dict.py
+++ data_science/python_part_basic/cookbook_practice/data_struct_algorithm/17_get_subset_from_dict.py
@@ -21,11 +21,17 @@ def get_subset_dict():
     p1 = {key: value for key, value in prices.items() if value > 200}
     print(p1)
     # {'AAPL': 612.78, 'IBM': 205.55}
+    p1 = ((key, value) for key, value in prices.items() if value > 200)
+    print(list(p1))
+    # [('AAPL', 612.78), ('IBM', 205.55)]
 
     names = ['IBM', 'HPQ', 'FB']
     p2 = {key: value for key, value in prices.items() if key in names}
     print(p2)
     # {'IBM': 205.55, 'HPQ': 37.2, 'FB': 10.75}
+    p2 = {key:prices[key] for key in prices.keys() & names}
+    print(p2)
+    # {'FB': 10.75, 'IBM': 205.55, 'HPQ': 37.2}
 
 
 if __name__ == '__main__':
